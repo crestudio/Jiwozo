8f922bcb914c56ddc2c7195f22c544fd7b8e0d6a
Macchiato <simppp@gmail.com>
2023-06-15 05:35:40 +0900

    리포지토리 초기화

---
+++ Assets/Editor/MissingScriptJiwozo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+namespace com.vrsuya.jiwozo {
+
+    public class MissingScriptJiwozo : EditorWindow {
+
+        [MenuItem("Tools/VRSuya/Jiwozo/Select All GameObject of Missing Script")]
+		/// <summary>Scene에 존재하는 Missing 컴포넌트를 포함한 GameObject를 선택합니다</summary>
+		public static void SelectMissingGameObjects() {
+            Component[] MissingComponents = GetAllMissingComponents();
+            if (MissingComponents.Length > 0) {
+                GameObject[] MissingGameObjects = MissingComponents.Select(TargetComponent => TargetComponent.gameObject).ToArray();
+				Selection.objects = MissingGameObjects;
+                Debug.Log("[VRSuya] " + MissingComponents.Length + " GameObjects have Missing Component Selected");
+			} else {
+				Debug.Log("[VRSuya] Not found GameObject has Missing Component");
+			}
+            return;
+        }
+
+		[MenuItem("Tools/VRSuya/Jiwozo/Remove All Missing Script Component")]
+		/// <summary>Scene에 존재하는 Missing 컴포넌트를 포함한 GameObject를 선택합니다</summary>
+		public static void RemoveMissingComponents() {
+			Component[] MissingComponents = GetAllMissingComponents();
+			if (MissingComponents.Length > 0) {
+				for (int Index = 0; Index < MissingComponents.Length; Index++) {
+					DestroyImmediate(MissingComponents[Index]);
+				}
+				Debug.Log("[VRSuya] " + MissingComponents.Length + " Missing Script Components Removed");
+			} else {
+				Debug.Log("[VRSuya] Not found Missing Script Component");
+			}
+			return;
+		}
+
+		/// <summary>Scene에 존재하는 Missing 컴포넌트 배열을 반환합니다</summary>
+		/// <returns>Scene에 존재하는 모든 Missing 컴포넌트 배열</returns>
+		private static Component[] GetAllMissingComponents() {
+			Component[] AllComponents = SceneManager.GetActiveScene().GetRootGameObjects().SelectMany(gameObject => gameObject.GetComponentsInChildren<Component>(true)).ToArray();
+            Component[] MissingComponents = Array.FindAll(AllComponents, TargetComponent => TargetComponent == null).ToArray();
+            return MissingComponents;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Jiwozo
-Unity 컴포넌트 삭제 보조 프로그램
+
+![Component](https://github.com/crestudio/Jiwozo/blob/master/Image/VRSuya_Jiwozo.jpg?raw=true)
+
+## 다운로드
+
+[VCC 패키지 추가](https://crestudio.notion.site/)
+
+[UnityPackage 다운로드](https://github.com/crestudio/Jiwozo/releases)
+
+---
+
+## 개요
+
+ Unity 컴포넌트 삭제 보조 프로그램
+
+- 개요
+
+---
+
+## 사용방법
+
+1. 사용방법
+
+---
+
+## 작동조건
+
+- [x] 작동조건
+
+---
+
+## 알려진 버그
+
+- [ ] 알려진 버그
+
+---
+
+## Contact
+
+- Twitter : https://twitter.com/VRSuya
+- Mail : vrsuya@gmail.com
